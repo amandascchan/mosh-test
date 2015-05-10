f759ed14e8288ea95251f0c375830ef9155b186b
Amanda Chan <amandachan@amandachan-mbp.local>
2015-05-10 14:22:23 -0700

    Initial Commit

---
+++ mosh-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mosh-test
+//
+//  Created by Amanda Chan on 5/10/15.
+//  Copyright (c) 2015 Amanda Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
