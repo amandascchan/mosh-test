ea2253c6192a20f8aedb37b5f4c4fd1cc1238727
Amanda Chan <amandachan@amandachan-mbp.local>
2015-05-31 14:00:34 -0700

    Initial Commit

---
+++ test_for_lols/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_for_lols
+//
+//  Created by Amanda Chan on 5/31/15.
+//  Copyright (c) 2015 Amanda Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
